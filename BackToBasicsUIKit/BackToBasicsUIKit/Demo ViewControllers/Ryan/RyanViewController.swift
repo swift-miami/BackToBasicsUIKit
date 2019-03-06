c04f8328b8ff719c6be9d3a78aeb26f87086756e
ryantstone <ryan@ryanstone.io>
2019-03-06 12:10:36 -0500

    Add alert actions

--- BackToBasicsUIKit/BackToBasicsUIKit/Demo ViewControllers/Ryan/RyanViewController.swift
+++ BackToBasicsUIKit/BackToBasicsUIKit/Demo ViewControllers/Ryan/RyanViewController.swift
@@ -29,7 +29,7 @@ class RyanViewController: UIViewController {
         collectionView.delegate    = self
         
         // Lets get some data!
-        let networking             = JSONPlaceholderNetworking()
+        let networking = JSONPlaceholderNetworking()
         networking.getImages(10) { self.images = $0 }
     }
 }
@@ -54,5 +54,29 @@ extension RyanViewController: UICollectionViewDataSource {
 }
 
 extension RyanViewController: UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Going to need the data on the image
+        let image = images[indexPath.row]
+        
+        // Going to determine if the current item number in the collection is even or odd to show the 2 different types of alert controller
+        let style: UIAlertController.Style = (indexPath.row % 2) == 0 ? UIAlertController.Style.actionSheet : UIAlertController.Style.alert
+        
+        // Going to initialize the alert controller to display the 
+        let alert = UIAlertController(title: String(describing: image.albumId), message: image.title, preferredStyle: style)
+        
+        // UIAlertAction takes a handler, this would be how you would respond to button presses
+        // So if a user presses OK and you need to do an action from there this is where you would do define that logic
+        // In this case we'll just print the title of the alert action
+        let printTitle: (UIAlertAction) -> () = { print($0.title!) }
+        
+        // There are 3 types of UIAlertAction OK, DEFAULT & DESTRUCTIVE
+        // Dev must add the action to the alert
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: printTitle))
+        alert.addAction(UIAlertAction(title: "DEFAULT", style: .default, handler: printTitle))
+        alert.addAction(UIAlertAction(title: "DESTRUCTIVE", style: .destructive, handler: printTitle))
+        
+        // Present the alert to be displayed in the current vc
+        present(alert, animated: true, completion: nil)
+    }
 }
