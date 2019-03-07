290623fdf94ad13262ac0b9dcc2f9dea1f8f887c
Alfredo Merino <alfimerino@gmail.com>
2019-03-06 23:53:53 -0500

    UIStepper Part 2: (#8)
    * UIStepper Part 2:
    - Added constraints to view controller with stacks
    - Removed extra labels
    
    * Formatting edits:
    - Erased extra spaces between methods
    - Removed  stackView.invalidateIntrinsicContentSize()

--- BackToBasicsUIKit/BackToBasicsUIKit/Demo ViewControllers/Freddy/FreddyViewController.swift
+++ BackToBasicsUIKit/BackToBasicsUIKit/Demo ViewControllers/Freddy/FreddyViewController.swift
@@ -14,30 +14,24 @@
 import UIKit
 
 class FreddyViewController: UIViewController {
+    
+    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var textView1: UITextView!
-    
     @IBOutlet weak var textView2: UITextView!
-   
     @IBOutlet weak var textView3: UITextView!
-    
     @IBOutlet weak var textView4: UITextView!
     
     // Stepper 1:
-    @IBOutlet weak var stTextView1: UITextView!
-    @IBOutlet weak var lable1: UILabel!
+    @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var stepper1: UIStepper!
     
-    
     // Stepper 2:
-    @IBOutlet weak var stTextView2: UITextView!
-    @IBOutlet weak var lable2: UILabel!
+    @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var stepper2: UIStepper!
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // textView 1
         textView1.text = "Text View 1: This view will show a standard green background color and purple text color... BORING!!!"
         textView1.backgroundColor = UIColor.green
@@ -63,30 +57,26 @@ class FreddyViewController: UIViewController {
         textView4.dataDetectorTypes = UIDataDetectorTypes.phoneNumber
         
         // Stepper 1: Customization
-        stTextView1.text = "In steps of 5. Max Val: 20, Min Val: -5"
         stepper1.maximumValue = 20
         stepper1.minimumValue = -5
-        lable1.text = "\(self.stepper1.value)" //Displays the starting value of stepper.
+        label1.text = "\(self.stepper1.value)" //Displays the starting value of stepper.
         
         // Stepper 2: Customization
-        stTextView2.text = "In Steps of 2. Max val: 32. Wraps around! "
         stepper2.maximumValue = 32
         stepper2.stepValue = 2
         stepper2.wraps = true
-        lable2.text = "\(self.stepper2.value)"
-        
+        label2.text = "\(self.stepper2.value)"
     }
+    
     // Stepper 1 Action
     @IBAction func stepperTapped1(_ sender: UIStepper) {
         // Connects stepper to label
-        self.lable1.text = "\(self.stepper1.value)"
-        
+        self.label1.text = "\(self.stepper1.value)"
     }
+    
     // Stepper 2 Action
     @IBAction func stepperTapped2(_ sender: UIStepper) {
         // Connects stepper to label
-        self.lable2.text = "\(self.stepper2.value)"
-        
+        self.label2.text = "\(self.stepper2.value)"
     }
-    
 }
