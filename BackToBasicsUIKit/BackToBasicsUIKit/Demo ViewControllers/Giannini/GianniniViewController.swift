ecbb11b652dabff1791b49e428f6527e30512ed8
Giannini Charles <gcharles@oceanreef.com>
2019-03-07 12:52:46 -0500

    Added Giannini files

--- BackToBasicsUIKit/BackToBasicsUIKit/Demo ViewControllers/Giannini/GianniniViewController.swift
+++ BackToBasicsUIKit/BackToBasicsUIKit/Demo ViewControllers/Giannini/GianniniViewController.swift
@@ -1,19 +1,43 @@
 /*
  - UITextField
-    - How do you customize: text, font, text color (both text and placeholder), background color, border color.
-    - How do you conform to its protocol.
+ - How do you customize: text, font, text color (both text and placeholder), background color, border color.
+ - How do you conform to its protocol.
  - UISlider
-    - How do you customize it
-    - How do you set minimum and maximum values
-    - How do you set a value
+ - How do you customize it
+ - How do you set minimum and maximum values
+ - How do you set a value
  */
 
 import UIKit
 
 class GianniniViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        edgesForExtendedLayout = []
         super.viewDidLoad()
-
+    }
+    
+    @IBAction func didTouchButton(_ sender: UIButton) {
+        var gianniniVC = UIViewController()
+        switch sender.tag {
+        case 1:
+            gianniniVC = IBSliderVC()
+            gianniniVC.navigationItem.title = "IB Slider"
+            
+        case 2:
+            gianniniVC = ProgrammaticSliderViewController()
+            gianniniVC.navigationItem.title = "Programmatic Slider"
+            
+        case 3:
+            gianniniVC = IBTextFieldViewController()
+            gianniniVC.navigationItem.title = "IB TextField"
+            
+        case 4:
+            gianniniVC = ProgrammaticTextFieldViewController()
+            gianniniVC.navigationItem.title = "Programmatic TextField"
+            
+        default: break
+        }
+        navigationController?.pushViewController(gianniniVC, animated: true)
     }
 }
---
+++ BackToBasicsUIKit/BackToBasicsUIKit/Demo ViewControllers/Giannini/ProgrammaticExamples/ProgrammaticSliderViewController.swift
@@ -0,0 +1,76 @@
+/*
+- UISlider
+    - How do you customize it
+- How do you set minimum and maximum values
+- How do you set a value
+*/
+
+import UIKit
+import AVFoundation
+
+class ProgrammaticSliderViewController: UIViewController {
+    
+    let slider = UISlider()
+    var imageView : UIImageView!
+    var songPlayer: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupSlider()
+        setupBackground()
+        layoutConstraints()
+        }
+    
+    override func viewDidAppear(_ animated: Bool) {
+         self.playSong()
+    }
+    
+    func setupSlider() {
+        view.addSubview(slider)
+        slider.addTarget(self, action: #selector(self.changeSliderValue(_:)), for: .valueChanged)
+        slider.minimumValue = 0.0
+        slider.maximumValue = 1.0
+        slider.value = 1
+        slider.maximumTrackTintColor = .black
+        slider.thumbTintColor = .blue
+        slider.isContinuous = true
+    }
+    
+    func layoutConstraints() {
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        slider.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        slider.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        slider.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    @objc func changeSliderValue(_ sender: UISlider) {
+        songPlayer?.volume = slider.value
+    }
+    
+    func setupBackground(){
+        let background = UIImage(named: "moonLake")
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.insertSubview(imageView, at: 0)
+    }
+    
+    func playSong() {
+        guard let url = Bundle.main.url(forResource: "kaiEngelMoonlight", withExtension: "mp3") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            songPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let songPlayer = songPlayer else { return }
+            songPlayer.play()
+        } catch _ {
+            return
+        }
+    }
+}
+
+
